bf24a927c37c860ee7c137dde6327f7026d8cbbf
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-30 11:34:38 -0400

    lamdas and streams arent my friend :(

--- src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
@@ -1,6 +1,10 @@
 package rocks.zipcode.io.assessment4.generics;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author leon on 09/12/2018.
@@ -13,10 +17,46 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
+        Map<SomeType,Integer> map = new HashMap<>();
+        for (SomeType element: array) {
+            if(!map.containsKey(element))
+            {
+                map.put(element,1);
+            } else
+                {
+                    map.replace(element, map.get(element)+1);
+                }
+        }
+
+        for (Map.Entry<SomeType,Integer> elem : map.entrySet()) {
+            if (elem.getValue() % 2 != 0){
+                return elem.getKey();
+            }
+        }
+
         return null;
-    }
+        }
+
+
 
     public SomeType findEvenOccurringValue() {
+        Map<SomeType,Integer> map = new HashMap<>();
+        for (SomeType element: array) {
+            if(!map.containsKey(element))
+            {
+                map.put(element,1);
+            } else
+            {
+                map.replace(element, map.get(element)+1);
+            }
+        }
+
+        for (Map.Entry<SomeType,Integer> elem : map.entrySet()) {
+            if (elem.getValue() % 2 == 0){
+                return elem.getKey();
+            }
+        }
+
         return null;
     }
 
@@ -29,6 +69,9 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
+
+
+//        return Arrays.stream(array).filter(SomeType -> predicate.equals());
         return null;
     }
 }
